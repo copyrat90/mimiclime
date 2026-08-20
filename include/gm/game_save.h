fc2cb99a2b700c78e0b3e905956a28c0ed37ef5a
copyrat90 <copyrat90@gmail.com>
2026-08-20 16:48:22 +0900

    Add game flag to `gm::game_save`

--- include/gm/game_save.h
+++ include/gm/game_save.h
@@ -2,6 +2,9 @@
 
 #include "gm/cfg/room_entrance.h"
 
+#include <bn_array.h>
+#include <bn_bitset.h>
+
 #include "ldtk_gen_enums.h"
 
 namespace ibn
@@ -16,12 +19,22 @@ namespace mc::gm
 
 struct game_save final
 {
+public:
     const unsigned slot;
 
     cfg::room_entrance room_entrance;
     ldtk::gen::species_kind player_species;
 
+private:
+    static constexpr int MAX_GAME_FLAG_COUNT = 256;
+    bn::array<bn::bitset_ref::element_t, MAX_GAME_FLAG_COUNT / 8> _game_flag_raw;
+    bn::bitset_ref _game_flag;
+
+public:
     game_save(unsigned slot);
+
+    bool game_flag(ldtk::gen::game_flag) const;
+    void set_game_flag(ldtk::gen::game_flag, bool value);
 
     void reset();
 
